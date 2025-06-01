36dcc7ed15e0225e4c9c6383214b01efa41d66f9
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-06-01 14:54:33 +0500

    lesson_52: главная страница и меню

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import index, master_detail, master_list, order_list
+from core.views import index, landing, master_detail, master_list, order_list
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index),
+    path("", landing, name="landing"),
+    path("index/", index, name="index"),
     path("masters/", master_list, name="master_list"),
-    path("masters/<int:master_id>/", master_detail),
+    path("masters/<int:master_id>/", master_detail, name="master_detail"),
     path("orders/", order_list, name="order_list"),
-
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/views.py
+++ core/views.py
@@ -5,24 +5,42 @@ from django.http import HttpResponse
 from .data import *
 
 
+def get_main_menu():
+    """Функция возвращает список пунктов меню для сайта"""
+    return [
+        {"name": "Главная", "url": "/"},
+        {"name": "О нас", "url": "#about"},
+        {"name": "Преимущества", "url": "#benefits"},
+        {"name": "Мастера", "url": "#masters"},
+        {"name": "Услуги", "url": "#services"},
+        {"name": "Запись", "url": "#booking"},
+        {"name": "Управление", "url": "/orders/"},
+    ]
+
+
+def landing(request):
+    """Главная страница сайта - лендинг"""
+    context = {
+        "title": "Барбершоп - стрижки и бритье",
+        "menu": get_main_menu(),
+        "masters": masters[:3],  # Показываем только первые 3 мастера
+        "services": services,
+    }
+    return render(request, "landing.html", context)
+
+
 def order_list(request):
     context = {
         "orders": orders,
         "title": "Список заявок",
+        "menu": get_main_menu(),
     }
     return render(request, "order_list.html", context)
 
 
 def index(request):
-    context = {
-        "user": {
-            "name": "Сергей",
-            "age": 30,
-            "is_stuff": True,
-        },
-        "md": markdown_string,
-    }
-    return render(request, "first_template.html", context)
+    # Перенаправляем на лендинг
+    return landing(request)
 
 
 def master_detail(request, master_id):
@@ -34,6 +52,8 @@ def master_detail(request, master_id):
 
     context = {
         "master": master,
+        "title": f"Мастер {master['name']}",
+        "menu": get_main_menu(),
     }
 
     return render(request, "master_detail.html", context)
@@ -42,5 +62,7 @@ def master_detail(request, master_id):
 def master_list(request):
     context = {
         "masters": masters,
+        "title": "Наши мастера",
+        "menu": get_main_menu(),
     }
     return render(request, "master_list.html", context)
