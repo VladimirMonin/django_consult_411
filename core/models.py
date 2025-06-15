4d39bab5a2bef0d82115dd1ada538e71b1a5ee69
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-06-15 12:29:26 +0500

    lesson_56: модель Review

--- core/models.py
+++ core/models.py
@@ -7,8 +7,11 @@ class Master(models.Model):
     middle_name = models.CharField(
         null=True, blank=True, max_length=100, verbose_name="Отчество"
     )
-    phone = models.CharField(max_length=20, verbose_name="Телефон", default="00000000000")
+    phone = models.CharField(
+        max_length=20, verbose_name="Телефон", default="00000000000"
+    )
     email = models.EmailField(null=True, blank=True, verbose_name="Email")
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
@@ -24,11 +27,41 @@ class Order(models.Model):
     name = models.CharField(max_length=100, verbose_name="Имя")
     phone = models.CharField(max_length=20, verbose_name="Телефон")
     comment = models.TextField(verbose_name="Комментарий", null=True, blank=True)
-    master = models.ForeignKey(Master, verbose_name="Мастер", default=None, on_delete=models.SET_DEFAULT, related_name="orders")
+    master = models.ForeignKey(
+        Master,
+        verbose_name="Мастер",
+        default=None,
+        on_delete=models.SET_DEFAULT,
+        related_name="orders",
+    )
 
 
-# Простая выборка в shell plus
-# orders = Order.objects.all() - получаем QuerySet всех объектов - это служебная коллекция
-# Если попростить orders[0] - Мы получаем из коллекции экземпляр Order
-# orders[0].name - Фродо. Получили данные из поля
-# orders[0].master.first_name - Получаем данные из связанной модели
+"""
+**Review (Отзыв)**
+
+- `text`: TextField (verbose_name="Текст отзыва")
+- `client_name`: CharField (max_length=100, blank=True, verbose_name="Имя клиента")
+- `master`: ForeignKey (на мастера, on_delete=models.CASCADE, verbose_name="Мастер")
+- `photo`: ImageField (upload_to="reviews/", blank=True, null=True, verbose_name="Фотография")
+- `created_at`: DateTimeField (auto_now_add=True, verbose_name="Дата создания")
+- `rating`: PositiveSmallIntegerField (с валидаторами MinValueValidator(1) и MaxValueValidator(5), verbose_name="Оценка") (Или через CHOICES)
+- `is_published`: BooleanField (default=True, verbose_name="Опубликован")
+"""
+
+
+class Review(models.Model):
+    text = models.TextField(verbose_name="Текст отзыва")
+    client_name = models.CharField(
+        max_length=100, blank=True, null=True, verbose_name="Имя клиента"
+    )
+    master = models.ForeignKey(
+        Master, on_delete=models.SET_NULL, null=True, verbose_name="Мастер"
+    )
+    photo = models.ImageField(
+        upload_to="reviews/", blank=True, null=True, verbose_name="Фотография"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    rating = models.PositiveSmallIntegerField(
+        verbose_name="Оценка", min_value=1, max_value=5
+    )
+    is_published = models.BooleanField(default=False, verbose_name="Опубликован")
