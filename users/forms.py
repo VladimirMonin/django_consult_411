71e5a71428aed8fb6ee3b1f3650f0b8feeea7dac
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-07-13 20:16:02 +0500

    fix: форма регистрации и авторизации

--- users/forms.py
+++ users/forms.py
@@ -1,67 +1,40 @@
 from django import forms
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
 
-class RegistrationForm(forms.Form):
-    username = forms.CharField(
-        label="Имя пользователя",
-        widget=forms.TextInput(attrs={"class": "form-control"}),
-    )
+class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(
         label="Email",
-        widget=forms.EmailInput(attrs={"class": "form-control"}),
-    )
-    password = forms.CharField(
-        label="Пароль",
-        widget=forms.PasswordInput(attrs={"class": "form-control"}),
-    )
-    password2 = forms.CharField(
-        label="Повторите пароль",
-        widget=forms.PasswordInput(attrs={"class": "form-control"}),
+        required=True,
+        widget=forms.EmailInput(attrs={"autocomplete": "email"}),
     )
 
-    def clean_username(self):
-        username = self.cleaned_data.get("username")
-        if User.objects.filter(username=username).exists():
-            raise forms.ValidationError("Пользователь с таким именем уже существует.")
-        return username
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Пользователь с таким email уже существует.")
+            raise ValidationError("Пользователь с таким email уже существует.")
         return email
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        password2 = cleaned_data.get("password2")
-        if password and password2 and password != password2:
-            raise forms.ValidationError("Пароли не совпадают.")
-        return cleaned_data
 
-
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        label="Имя пользователя",
-        widget=forms.TextInput(attrs={"class": "form-control"}),
-    )
-    password = forms.CharField(
-        label="Пароль",
-        widget=forms.PasswordInput(attrs={"class": "form-control"}),
-    )
-
-    def clean(self):
-        cleaned_data = super().clean()
-        username = cleaned_data.get("username")
-        password = cleaned_data.get("password")
-
-        if username and password:
-            user = authenticate(username=username, password=password)
-            if user is None:
-                raise forms.ValidationError("Неверное имя пользователя или пароль.")
-            # Сохраняем пользователя в форме для последующего использования во вью
-            self.user_cache = user
-        return cleaned_data
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Имя пользователя"}
+        )
+        self.fields["password"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Пароль"}
+        )
--- users/views.py
+++ users/views.py
@@ -1,43 +1,34 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model, login as auth_login, logout as auth_logout
+from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.views.decorators.http import require_POST
-from .forms import RegistrationForm, LoginForm
-
-
-User = get_user_model()
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
 
 def register(request):
     if request.method == "POST":
-        form = RegistrationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(
-                username=form.cleaned_data["username"],
-                email=form.cleaned_data["email"],
-                password=form.cleaned_data["password"],
-            )
+            user = form.save()
             auth_login(request, user)
             return redirect("landing")
     else:
-        form = RegistrationForm()
+        form = CustomUserCreationForm()
     return render(request, "users/register.html", {"form": form})
 
 
 def login(request):
     if request.method == "POST":
-        form = LoginForm(request.POST)
+        form = CustomAuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.user_cache
-            if user is not None:
-                auth_login(request, user)
-                # Перенаправляем на next, если он есть
-                return redirect(request.GET.get("next", "landing"))
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect(request.GET.get("next", "landing"))
     else:
-        form = LoginForm()
+        form = CustomAuthenticationForm()
     return render(request, "users/login.html", {"form": form})
 
 
-@require_POST # Будут работать только POST-запросы!
+@require_POST
 def logout(request):
     auth_logout(request)
     return redirect("landing")
