0c6b129f1b4546307bce71b23e3f629de520cc45
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-08-02 18:42:54 +0500

    jazzmin - попробовал отключить

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -35,7 +35,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "jazzmin",
+    # "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
